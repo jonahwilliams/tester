8ac68f891773e4277acab171891ca917fb41590c
Jonah Williams <campfish91@gmail.com>
2020-05-26 12:51:11 -0700

    remove tester_test dependency

--- test/trivial_test.dart
+++ test/trivial_test.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:tester_test/tester_test.dart';
-
 /// Test a simple expectation.
 void testStopThrow() {
-  expect(2 + 1, 3);
+  assert(2 + 1 == 3);
 }
