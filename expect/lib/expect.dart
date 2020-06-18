779cd65baaf465d198bfeae018b9f11ae9c64d8a
Jonah Williams <jonahwilliams@google.com>
2020-06-18 12:16:24 -0700

    Move package:expect back to package:matcher (#51)
    I'm probably not going to be able to make drastic improvements to the expectation library.
    
    Also fix a bug in bin/tester wrt the current directory and set of tests that get run.

--- expect/lib/expect.dart
+++ expect/lib/expect.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'src/matcher.dart';
-export 'src/matcher.dart';
+import 'package:matcher/matcher.dart';
+export 'package:matcher/matcher.dart';
 
 /// Assert that [actual] matches [matcher].
 ///
